e75ce551b5f73ea628e7ffc7510fabf637917cd5
Lilla Szekesi <szekesililla93@gmail.com>
2018-03-07 14:21:14 +0100

    Example printing in main

--- com/codecool/quiz_game.java
+++ com/codecool/quiz_game.java
@@ -11,8 +11,8 @@ import java.util.*;
 public class quiz_game {
     public static void main(String[] args) {
         Terminal term = new Terminal();
-        ReadFile read = new ReadFile();
+        /* ReadFile read = new ReadFile();
         ArrayList<HashMap<String, String>> questions = read.questions();
-        System.out.println(questions.get(0));
+        System.out.println(questions.get(0)); */
     }
 }
