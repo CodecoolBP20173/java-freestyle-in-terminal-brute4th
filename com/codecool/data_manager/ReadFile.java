326d79c127e107194b3b4e4eb37bb9726ce1eae2
Lilla Szekesi <szekesililla93@gmail.com>
2018-03-07 12:41:15 +0100

    Generate ArrayList of question HashMaps

--- com/codecool/data_manager/ReadFile.java
+++ com/codecool/data_manager/ReadFile.java
@@ -1,8 +1,7 @@
 package com.codecool.data_manager;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.ArrayList;
+import java.util.*;
 
 public class ReadFile {
     public ArrayList<String> readFile(String file) {
@@ -30,11 +29,8 @@ public class ReadFile {
             ArrayList<String> dataFromFile = new ArrayList<String>();
 
             while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
                 dataFromFile.add(line);
             }   
-
-            System.out.println(dataFromFile);
         
             // Always close files.
             bufferedReader.close();    
@@ -52,9 +48,27 @@ public class ReadFile {
             // Or we could just do this: 
             // ex.printStackTrace();
         }
+
     ArrayList<String> datat = new ArrayList<String>();
     String str="vmi";
     datat.add(str);
     return datat;
+    }
+
+    public  ArrayList<HashMap<String, String>> questions() {
+        ArrayList<String> questionList = readFile("questions");
+        int length = questionList.size()/6;
+        ArrayList<HashMap<String, String>> questions = new ArrayList<HashMap<String, String>>();
+
+        for (int i = 0; i < length; i++) {
+            HashMap<String, String> question = new HashMap<String, String>();
+            question.put("question",new String(questionList.get(1+(i*6))));
+            question.put("aGood", questionList.get(2+(i*6)));
+            question.put("a1", questionList.get(3+(i*6)));
+            question.put("a2", questionList.get(4+(i*6)));
+            question.put("a3", questionList.get(5+(i*6)));
+            questions.add(question);
+        }
+        return questions;
     }
 }
--- com/codecool/quiz_game.java
+++ com/codecool/quiz_game.java
@@ -4,6 +4,7 @@ import com.codecool.termlib.Terminal;
 import com.codecool.termlib.Color;
 import com.codecool.termlib.Direction;
 import com.codecool.data_manager.*;
+import java.util.*;
 
 
 
@@ -11,6 +12,7 @@ public class quiz_game {
     public static void main(String[] args) {
         Terminal term = new Terminal();
         ReadFile read = new ReadFile();
-        read.readFile("questions");
+        ArrayList<HashMap<String, String>> questions = read.questions();
+        System.out.println(questions.get(0));
     }
 }
